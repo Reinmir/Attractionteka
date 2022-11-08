6d106c677b12994d0be363c2cee76573d0a179c0
reinmir <studenko.andrey@gmail.com>
2022-11-08 10:37:14 +0200

    Some bugs fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { LogoIcon } from "./HeaderIcons";
+import { logoIcon, logOutIcon } from "./HeaderIcons";
 import "./style.scss";
 
 export const Header: React.FC = () => {
   return (
     <div className="header__container">
-      <div className="header__logoIcon">{LogoIcon}</div>
+      <div className="header__logoIcon">{logoIcon}</div>
+      <div className="header__logout">{logOutIcon}</div>
     </div>
   );
 };
